02add3c2e60a52c12878f308a8ff44d5ab9bca6c
jh5113HJ <jh5113@columbia.edu>
2026-02-03 12:12:49 -0500

    Update HW 1 - Practicing Commits .py
    Second modification to Python script

--- homework-1-main/Submissions/HUANG-JIAN/HW 1 - Practicing Commits .py
+++ homework-1-main/Submissions/HUANG-JIAN/HW 1 - Practicing Commits .py
@@ -6,3 +6,7 @@ print("This is my first Git commit practice file")
 
 # First modification - practicing Git commits
 print("This is my first modification!")
+
+# Second modification - learning version control
+commit_count = 2
+print(f"I have made {commit_count} modifications so far!")
